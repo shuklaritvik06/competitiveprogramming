a3028799d022e2d487e1668ecb5015c058a4a811
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-08-31 13:25:22 +0530

    Mathematics & Bitwise

---
+++ Code/src/com/kunal/mathematics/GraySimilarCode.java
@@ -0,0 +1,15 @@
+package com.kunal.mathematics;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class GraySimilarCode {
+
+    // Pigeon-Hole principle
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+    }
+}
---
+++ Code/src/com/kunal/mathematics/SEQ.java
@@ -0,0 +1,7 @@
+package com.kunal.mathematics;
+// https://www.spoj.com/problems/SEQ/
+public class SEQ {
+    public static void main(String[] args) {
+
+    }
+}
