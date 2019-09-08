0d359ad68f50a728a376b7a2cc9527740d70fd90
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-09-08 23:38:01 +0530

    code

--- Code/src/com/kunal/mathematics/GraySimilarCode.java
+++ Code/src/com/kunal/mathematics/GraySimilarCode.java
@@ -6,10 +6,16 @@ import java.util.Scanner;
 
 public class GraySimilarCode {
 
-    // Pigeon-Hole principle
+//    https://www.codechef.com/problems/GRAYSC
+//    Pigeon-Hole principle
+//    Gray code : two consecutive numbers will have only one bit different in their binary representation, for example - 1101 & 1001
+//    So the xor of two consecutive numbers in gray code will have only one 1 in it.
+
+    // if N >= 130, we will have 65 pairs (with only one set bit). Each int is 64 bit and hence one place will have 2 bits because numbers are 65.
+    // if N < 130 then apply brute-force
 
     public static void main(String[] args) {
         Scanner s = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-
+        
     }
 }
