fda45e25c64b4a2a6d9f01fb530fcf90bafa1f99
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-09-11 20:55:08 +0530

    Recursion & Backtracking

--- Code/src/com/kunal/recursionNbacktracking/SudokuSolver.java
+++ Code/src/com/kunal/recursionNbacktracking/SudokuSolver.java
@@ -1,7 +1,12 @@
 package com.kunal.recursionNbacktracking;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class SudokuSolver {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        
     }
 }
