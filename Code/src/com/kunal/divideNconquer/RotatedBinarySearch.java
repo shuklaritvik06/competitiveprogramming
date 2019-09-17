619bc64ed0f4a455a9ef57bcd198595dcfc8ae99
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-09-17 20:16:47 +0530

    divide n conquer

---
+++ Code/src/com/kunal/divideNconquer/RotatedBinarySearch.java
@@ -0,0 +1,5 @@
+package com.kunal.divideNconquer;
+
+public class RotatedBinarySearch {
+
+}
--- Code/src/com/kunal/recursionNbacktracking/Sagheer.java
+++ Code/src/com/kunal/recursionNbacktracking/Sagheer.java
@@ -1,9 +1,12 @@
-package com.company;
+package com.kunal.recursionNbacktracking;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+/*
+        3 4 001000 000010 000010
+ */
+public class Sagheer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -33,16 +36,26 @@ public class Main {
             }
         }
         int[][] build = Arrays.copyOfRange(building , topZeroRow , n);
-        System.out.println(caculate(n -1-topZeroRow, 0 , build));
+
+        for (int i = 0; i < build.length; i++) {
+            for (int j = 0; j < build[0].length; j++) {
+                System.out.print(build[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+//        System.out.println(topZeroRow);
+
+        System.out.println(calculate(n-1-topZeroRow, 0 , build));
     }
 
-    private static int caculate(int floor, int door, int[][] building) {
+    private static int calculate(int floor, int door, int[][] building) {
         if (floor==-1){
             return 0;
         }
 
         int cost = 0;
-        if (door ==0) {
+        if (door == 0) {
             for (int i = door; i < building[0].length; i++) {
                 if (building[floor][i] == 1) {
                     cost = i;
@@ -51,12 +64,13 @@ public class Main {
             if (floor == 0){
                 return cost;
             }
-            int left = caculate(floor-1 , 0 , building);
-            int right = caculate(floor-1 , building[0].length-1 , building);
-            return 1+Math.min(2*cost+ left, building[0].length-1+ right);
-        }
-        else {
-            for (int i = building[0].length-1; i >=0 ; i--) {
+            // Pre-compute position (index) of room with light on from left and right for each row
+            int left = calculate(floor-1 , 0 , building);
+            int right = calculate(floor-1 , building[0].length-1 , building);
+            return 1+Math.min(2*cost + left, building[0].length-1 + right);
+
+        } else {
+            for (int i = building[0].length-1; i >= 0 ; i--) {
                 if (building[floor][i] == 1){
                     cost = building[0].length -1- i;
                 }
@@ -64,8 +78,8 @@ public class Main {
             if (floor == 0){
                 return cost;
             }
-            int left = caculate(floor-1 , 0 , building);
-            int right = caculate(floor - 1 , building[0].length-1 , building);
+            int left = calculate(floor-1 , 0 , building);
+            int right = calculate(floor - 1 , building[0].length-1 , building);
             return 1 + Math.min( building[0].length -1 +  left, 2*cost+ right);
         }
     }
